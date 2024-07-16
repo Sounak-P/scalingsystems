cadc0ad881e1288fdf4ad37f407f06e4db2d9463
Sounak-P <sounak.p@outlook.com>
2024-07-16 21:58:10 +0530

    First Commit

--- tpot/tpot_titanic_pipeline.py
+++ tpot/tpot_titanic_pipeline.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import SelectFwe, f_classif
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.tree import DecisionTreeClassifier
+from tpot.builtins import StackingEstimator
+from xgboost import XGBClassifier
 
 # NOTE: Make sure that the outcome column is labeled 'target' in the data file
 tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
@@ -11,10 +12,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.8084390079676803
+# Average CV score on the training set was: 0.8412972730333296
 exported_pipeline = make_pipeline(
-    SelectFwe(score_func=f_classif, alpha=0.009000000000000001),
-    RandomForestClassifier(bootstrap=True, criterion="gini", max_features=0.8500000000000001, min_samples_leaf=9, min_samples_split=17, n_estimators=100)
+    StackingEstimator(estimator=DecisionTreeClassifier(criterion="entropy", max_depth=4, min_samples_leaf=1, min_samples_split=4)),
+    XGBClassifier(learning_rate=0.1, max_depth=10, min_child_weight=12, n_estimators=100, n_jobs=1, subsample=0.55, verbosity=0)
 )
 
 exported_pipeline.fit(training_features, training_target)
